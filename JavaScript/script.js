e7f6216010637bad5f2a6d0463500af852b74afa
GeekySazzad <71886049+sazzadalam112@users.noreply.github.com>
2023-09-27 00:30:35 +0530

    initial-commit

---
+++ JavaScript/script.js
@@ -0,0 +1,35 @@
+const navList = document.querySelector(".nav-list");
+const hamberger = document.querySelector(".hamberger");
+const year = new Date().getFullYear();
+const footer = document.getElementById("footer");
+
+const emailTo = `mdsazzadalamcp@gmail.com`;
+const senderName = document.getElementById("sender-name");
+const phoneNumber = document.getElementById("phone");
+const emailSubject = document.getElementById("subject");
+const message = document.getElementById("message-area");
+
+const sendBtn = document.getElementById("submit-btn");
+
+hamberger.addEventListener("click", (e) => {
+  navList.classList.toggle("active");
+  document.querySelector(".fa-bars").classList.toggle("fa-xmark");
+});
+
+window.onscroll = () => {
+  navList.classList.remove("active");
+  document.querySelector(".fa-bars").classList.remove("fa-xmark");
+};
+const ele = `<p>MSA &copy; ${year}</p>`;
+
+footer.innerHTML = ele;
+// Scrolling effect
+
+sendBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  window.open(
+    `mailto:${emailTo}?subject=${emailSubject.value}&body= ${'I am ' +senderName.value + ' ' +'phone : '+ phoneNumber.value+ ' ' + message.value}`,
+    "_self"
+  );
+});
